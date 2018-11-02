31e3eac2924bae25c728231cdd86181d3373ed9a
Orange-J <juhaotian@foxmail.com>
2018-11-03 00:56:49 +0800

    feat: 测试scss生效的简例

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 import Button from 'antd/lib/button';
-import './App.css';
+import './App.scss';
 
 class App extends Component {
     render() {
         return (
             <div className="App">
                 <Button type="primary">Test</Button>
+                <div className="scss-test">Sass-test</div>
             </div>
         );
     }
