cc95b1f6588e47e12120e3fdd27494efd6b29393
Orange-J <juhaotian@foxmail.com>
2018-11-10 17:08:32 +0800

    [feat] 首页

--- src/page/Home/index.js
+++ src/page/Home/index.js
@@ -1,9 +1,15 @@
 import React, { Component } from 'react';
+import styles from './index.module.scss';
 
 class Home extends Component {
     render () {
         return (
-           'Home'
+            <div className={styles.homepage}>
+                <div className={styles.textBox}>
+                    <div className={styles.title}>React for React</div>
+                    <div className={styles.introduce}>Something about ReactJS, written in ReactJS.</div>
+                </div>
+            </div>
         );
     }
 }
