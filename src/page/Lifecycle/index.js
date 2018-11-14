60ef19f2251e8f6a26d037ece463d923f9d277ff
Orange-J <juhaotian@foxmail.com>
2018-11-14 10:08:05 +0800

    [feat] lifecycle页面-chart的大致布局

--- src/page/Lifecycle/index.js
+++ src/page/Lifecycle/index.js
@@ -4,10 +4,15 @@ import styles from './index.module.scss';
 class Lifecycle extends Component {
     render () {
         return (
-            <div class={styles.lifecycle}>
-                Lifecycle
-
+            <div className={styles.lifecycle}>
                 {/* TODO: https://www.wikitechy.com/tutorials/react/reactjs-component-life-cycle */}
+                <div className={styles.chart}>
+                    <div className={styles.startBox}>startBox</div>
+                    <div className={styles.renderBox}>renderBox</div>
+                    <div className={styles.updateBox}>updateBox</div>
+                    <div className={styles.middleStateBox}>middleStateBox</div>
+                    <div className={styles.setStateBox}>setStateBox</div>
+                </div>
             </div>
         );
     }
