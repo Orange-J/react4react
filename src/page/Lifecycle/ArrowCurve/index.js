154baa1a98f5610dace42876f5ae99e2c0e8d060
Orange-J <juhaotian@foxmail.com>
2018-11-19 20:31:07 +0800

    [feat] ArrowCurve组件的propTypes和defaultProps

---
+++ src/page/Lifecycle/ArrowCurve/index.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styles from './index.module.scss';
+
+const ArrowCurve = props => (
+    <div className={`${styles.ArrowCurve} ${props.cls}`}>
+
+    </div>
+);
+
+ArrowCurve.propTypes = {
+    cls: PropTypes.string,
+    width: PropTypes.number,
+    height: PropTypes.number,
+    startXY: PropTypes.string,
+    endXY: PropTypes.string,
+    controlXY: PropTypes.string,
+    arrowAngle: PropTypes.number,
+    lineStyle: PropTypes.oneOf(['solid', 'dashed', 'dotted'])
+};
+
+ArrowCurve.defaultProps = {
+    cls: '',
+    width: 100,
+    height: 50,
+    startXY: '0 0',
+    endXY: '100 50',
+    controlXY: '0 50',
+    arrowAngle: 90,
+    lineStyle: 'solid'
+};
+
+export default ArrowCurve;
