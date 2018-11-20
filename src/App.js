95094067f5044eb2f399f0d8f56b43c4cadfd9ab
Orange-J <juhaotian@foxmail.com>
2018-11-20 20:38:18 +0800

    [feat] url变化与左Menu选中状态的联动

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { HashRouter } from 'react-router-dom';
+import { HashRouter, Route } from 'react-router-dom';
 import Header from './component/Header';
 import Sidebar from './component/Sidebar';
 import Main from './component/Main';
@@ -10,10 +10,19 @@ class App extends Component {
         return (
             <HashRouter>
                 <div className="App">
-                    <div className="header"><Header/></div>
+                    <div className="header"><Header /></div>
                     <div className="body">
-                        <div className="sidebar"><Sidebar/></div>
-                        <div className="main"><Main/></div>
+                        <div className="sidebar">
+                            {
+                            /**
+                             * 处理url变化与某组件状态联动的方案：
+                             * 用<Route>监听根路径并把component设为要处理的组件，会向component的props传入location对象
+                             * 每次url变化时，component所指向的组件内props.location.pathname都会变化
+                             */
+                            }
+                            <Route path="/" component={ Sidebar } />
+                        </div>
+                        <div className="main"><Main /></div>
                     </div>
                 </div>
             </HashRouter>
--- src/component/Sidebar/index.js
+++ src/component/Sidebar/index.js
@@ -24,13 +24,13 @@ class Sidebar extends Component {
     render () {
         return (
             <Menu
-                defaultSelectedKeys={['1']}
+                selectedKeys={[this.props.location.pathname]}
                 defaultOpenKeys={['sub1']}
                 mode="inline"
                 inlineCollapsed={this.state.collapsed}
             >
-                <Menu.Item key="1"><Link to="/">Home</Link></Menu.Item>
-                <Menu.Item key="2"><Link to="/lifecycle">Lifecycle</Link></Menu.Item>
+                <Menu.Item key="/"><Link to="/">Home</Link></Menu.Item>
+                <Menu.Item key="/lifecycle"><Link to="/lifecycle">Lifecycle</Link></Menu.Item>
                 <SubMenu key="sub1" title={'G1'}>
                     <Menu.Item key="3">Option 3</Menu.Item>
                     <Menu.Item key="4">Option 4</Menu.Item>
