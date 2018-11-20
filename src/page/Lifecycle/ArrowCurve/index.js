a0a714ece462d6feef7e055cbbfb699783c04910
Orange-J <juhaotian@foxmail.com>
2018-11-20 12:41:17 +0800

    [feat] ArrowCurve组件的箭头功能

--- src/page/Lifecycle/ArrowCurve/index.js
+++ src/page/Lifecycle/ArrowCurve/index.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import Arrow from '../Arrow';
 import styles from './index.module.scss';
 
 const lineStyleConfig = {
     solid: {
-        strokeWidth: 4
+        strokeWidth: 2
     },
     dashed: {
         strokeWidth: 2,
-        strokeDasharray: '1 1',
+        strokeDasharray: '5 6',
         strokeDashoffset: 2
     },
     dotted: {
@@ -22,6 +23,15 @@ const getCurvePath = ({startXY, endXY, controlXY}) => {
     return `M${startXY} Q${controlXY} ${endXY}`;
 };
 
+const getArrowWrapStyle = ({endXY, arrowAngle, arrowOffsetXY}) => {
+    endXY = endXY.split(/\s+/).map(Number);
+    return {
+        left: endXY[0] + arrowOffsetXY[0],
+        top: endXY[1] + arrowOffsetXY[1],
+        transform: `rotate(${arrowAngle}deg)`
+    };
+};
+
 const ArrowCurve = props => (
     <div className={`${styles.ArrowCurve} ${props.cls}`} style={{width: props.width, height: props.height}}>
         <svg width="100%" height="100%" version="1.1">
@@ -33,6 +43,15 @@ const ArrowCurve = props => (
                 {...lineStyleConfig[props.lineStyle]}
             ></path>
         </svg>
+        {props.showArrow ? (
+            <div
+                className={styles.arrowWrap}
+                style={getArrowWrapStyle(props)}
+            >
+                <Arrow direction="top" />
+            </div>
+        ) : null}
+        {props.children}
     </div>
 );
 
@@ -44,8 +63,10 @@ ArrowCurve.propTypes = {
     endXY: PropTypes.string,
     controlXY: PropTypes.string,
     showArrow: PropTypes.bool,
-    arrowAngle: PropTypes.number,
-    lineStyle: PropTypes.oneOf(['solid', 'dashed', 'dotted'])
+    arrowAngle: PropTypes.number, // 箭头默认指向上方，通过此选项控制箭头的顺时针旋转角度
+    arrowOffsetXY: PropTypes.array,
+    lineStyle: PropTypes.oneOf(['solid', 'dashed', 'dotted']),
+    children: PropTypes.node
 };
 
 ArrowCurve.defaultProps = {
@@ -56,7 +77,8 @@ ArrowCurve.defaultProps = {
     endXY: '100 50',
     controlXY: '0 50',
     showArrow: true,
-    arrowAngle: 90,
+    arrowAngle: 0,
+    arrowOffsetXY: [0, 0],
     lineStyle: 'solid'
 };
 
